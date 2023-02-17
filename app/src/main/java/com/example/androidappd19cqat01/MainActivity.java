f23f9937c9506768bec09408ebfa309ed1675a55
chitaitruong <chitaitruong.7@gmail.com>
2023-02-17 14:37:53 +0700

    add bai4

--- app/src/main/java/com/example/androidappd19cqat01/MainActivity.java
+++ app/src/main/java/com/example/androidappd19cqat01/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.androidappd19cqat01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,21 +10,29 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Bai 3
+    Button btnCong, btnTru, btnNhan, btnChia;
+    EditText edtSo1, edtSo2;
+    TextView tv1;
+    //Bai 4
+    Button btnDK;
+    EditText edtTK, edtMK, edtNS, edtEmail;
+    TextView tvTT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         //setContentView(R.layout.frame_layout_bai1);
         //setContentView(R.layout.linear_layout_bai2);
-        setContentView(R.layout.linear_layout_bai3);
-        setControl();
-        setEvent();
+        /*setContentView(R.layout.linear_layout_bai3);
+        setControlBai3();
+        setEventBai3();*/
+        setContentView(R.layout.linear_layout_bai4);
+        setControlBai4();
+        setEventBai4();
     }
-    Button btnCong, btnTru, btnNhan, btnChia;
-    EditText edtSo1, edtSo2;
-    TextView tv1;
-    private void setControl() {
+
+    private void setControlBai3() {
         edtSo1 = findViewById(R.id.edtSo1);
         edtSo2 = findViewById(R.id.edtSo2);
         tv1 = findViewById(R.id.tv1);
@@ -32,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         btnNhan = findViewById(R.id.btnNhan);
         btnChia = findViewById(R.id.btnChia);
     }
-    private void setEvent() {
+    private void setEventBai3() {
         btnCong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,6 +76,29 @@ public class MainActivity extends AppCompatActivity {
                 int so2 = Integer.parseInt(edtSo2.getText().toString());
                 float kq = so1 / so2;
                 tv1.setText(String.valueOf(kq));
+            }
+        });
+    }
+    private void setControlBai4() {
+        edtTK = findViewById(R.id.edtTK);
+        edtMK = findViewById(R.id.edtMK);
+        edtNS = findViewById(R.id.edtNS);
+        edtEmail = findViewById(R.id.edtEmail);
+        btnDK = findViewById(R.id.btnDK);
+        tvTT = findViewById(R.id.tvTT);
+    }
+    private void setEventBai4() {
+        btnDK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tk = edtTK.getText().toString();
+                String mk = edtMK.getText().toString();
+                String ns = edtNS.getText().toString();
+                String email = edtEmail.getText().toString();
+                String tt = tk + "\n" + mk + "\n" + ns + "\n" + email;
+                tvTT.setText(tt);
+                tvTT.setBackgroundColor(Color.GREEN);
+                tvTT.setTextSize(30);
             }
         });
     }
